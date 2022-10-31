f783086cf7080c1b99f7aa2b4fd7b2a3f72c26db
Anindya Lokeswara <anindya.lokeswara@ui.ac.id>
2022-10-31 23:47:30 +0700

    landingPage

---
+++ landingPage/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from landingPage.views import show_landingPage, show_afterLoginlandingPage, register, login_user, logout_user, add_feedback, delete_feedback, show_allFeedback_json, show_userFeedback_json
+
+app_name = 'landingPage'
+
+urlpatterns = [
+    path('', show_landingPage, name='show_landingPage'),
+    path('landingpage/', show_afterLoginlandingPage, name='show_afterLoginlandingPage'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login_user'),
+    path('logout/', logout_user, name='logout_user'),
+    path('add-feedback/', add_feedback, name='add_feedback'),
+    path('delete-task/<int:pk>', delete_feedback, name='delete_feedback'),
+    path('json-all/', show_allFeedback_json, name='show_allFeedback_json'),
+    path('json-user/', show_userFeedback_json, name='show_userFeedback_json'),
+]
