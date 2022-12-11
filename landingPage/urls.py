48488e90d674e1cffba394ff4147476f0bb8f5e4
Kathleen Daniella <kathleen.daniella@ui.ac.id>
2022-12-12 02:36:30 +0700

    json user

--- landingPage/urls.py
+++ landingPage/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from landingPage.views import show_landingPage, register, login_user, logout_user, add_feedback, delete_feedback, show_allFeedback_json, show_userFeedback_json
+from landingPage.views import show_landingPage, register, login_user, logout_user, add_feedback, delete_feedback, show_allFeedback_json, show_userFeedback_json, show_user_json
 
 from core.views import about, frontpage # dari aplikasi core (afiq)
 
@@ -16,4 +16,5 @@ urlpatterns = [
     path('json-user/', show_userFeedback_json, name='show_userFeedback_json'),
     path('blog/', frontpage, name='frontpage'), # dari aplikasi core --> halaman list artikel (afiq)
     path('blog/about', about, name='about'), # dari aplikasi core --> halaman about (afiq)
+    path('users/', show_user_json, name='show_user_json'),
 ]
